5ed36737fb72e418a238c0fce9fe8708a2e43b8e
artem <artem.mindsadyrov@actumdigital.com>
2024-12-16 11:53:57 +0100

    Initial commit

---
+++ src/test/java/org/example/demo/pages/SignUpPage.java
@@ -0,0 +1,9 @@
+package org.example.pages;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+public class SignUpPage {
+//    SelenideElement username = "a";
+//    SelenideElement Password = a;
+}
